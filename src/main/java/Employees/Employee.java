99f95d0af09a2cffa535436e9d292ed19235b352
kmbruce1986 <kmbruce1986@gmail.com>
2018-10-01 15:34:32 +0100

    creates abstract employee class

--- src/main/java/Employees/Employee.java
+++ src/main/java/Employees/Employee.java
@@ -1,4 +1,35 @@
 package Employees;
 
-public class Employee {
+public abstract class Employee {
+
+    private String name;
+    private String NInumber;
+    private double salary;
+
+    public Employee(String name, String NInumber, double salary) {
+        this.name = name;
+        this.NInumber = NInumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNInumber() {
+        return NInumber;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(Double raiseAmount){
+        this.salary = this.salary + raiseAmount;
+    }
+
+    public double payBonus(){
+        return this.salary / 100;
+    }
+
 }
