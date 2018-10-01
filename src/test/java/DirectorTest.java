6ba856a6cb9244054a89896b597b5f2e687a4af2
kmbruce1986 <kmbruce1986@gmail.com>
2018-10-01 16:19:19 +0100

    amends director payBonus to 2% - test passes

--- src/test/java/DirectorTest.java
+++ src/test/java/DirectorTest.java
@@ -52,7 +52,7 @@ public class DirectorTest {
 
     @Test
     public void canPayBonus() {
-        assertEquals(100000.00, director.payBonus(), 0.01);
+        assertEquals(200000.00, director.payBonus(), 0.01);
     }
 
     @Test
